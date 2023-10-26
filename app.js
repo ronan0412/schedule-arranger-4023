6af7f9ea961a82ad46fb8259f70ab7799928c43b
Kosuke <darthbenirece@gmail.com>
2023-10-27 00:25:10 +0900

    renderの環境変数を利用できるように修正2

--- app.js
+++ app.js
@@ -25,8 +25,8 @@ User.sync().then(async () => {
 });
 
 const GitHubStrategy = require('passport-github2').Strategy;
-const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '660eeeaf5326fdb402c9';
-const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '64043b205de580869b082e111d8b18b88ade8d52';
+const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID || '2f831cb3d4aac02393aa';
+const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET || '9fbc340ac0175123695d2dedfbdf5a78df3b8067';
 
 passport.serializeUser(function (user, done) {
   done(null, user);
